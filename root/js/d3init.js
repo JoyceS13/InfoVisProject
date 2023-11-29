165cb22cf14f060e26aef2cb586a2b9bce1b0b3a
Jasper Vos <23446038+Jasper-Vos@users.noreply.github.com>
2023-11-29 22:28:53 +0100

    Added basic file/folderstructure, code basis

---
+++ root/js/d3init.js
@@ -0,0 +1,20 @@
+// d3init.js - Initialization file for D3.js setup and CSV loading
+
+// init d3, load the data file
+function initialize() {
+    // Load CSV file
+    d3.csv('data/Spotify_Youtube.csv')
+        .then(function (data) {
+            // Log data 
+            console.log('CSV data:', data);
+        })
+        .catch(function (error) {
+            // Handle errors if the file fails to load
+            console.error('Error loading the CSV file:', error);
+        });
+}
+
+// Call the initialization function when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    initialize(); // Call the initialize function when the DOM is loaded
+})
