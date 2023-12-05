a0dcbe8858770a9bf11b757e3352871040273a46
Joyce Sung <wyjsung@gmail.com>
2023-12-05 22:36:40 +0100

    added radio buttons, restructured info cards

--- root/js/info_card.js
+++ root/js/info_card.js
@@ -1,40 +1,110 @@
 class InfoCard{
-    constructor(container, type, data){
-        this.container = container;
+    constructor(id, type){
+        this.id = id;
         this.type = type;
-        this.data = data;
+        this.data;
 
-        var infocard = d3.select(container).append("div").attr("class", "info_card");
-        var infocard_header = infocard.append("div").attr("class", "info_card_header");
-        var info_card_body = infocard.append("div").attr("class", "info_card_body");
+        this.update_info(id)
+    }   
 
-        if(type==="song"){
-            this.add_song_info(infocard);
-        } else if (type === "artist"){
-            this.add_artist_info(infocard);
+    //updates the information
+    update_info(){
+        var info_card = d3.select(this.id)
+
+        let header_text
+        let body_text
+
+        //adds information depending on whether an artist or song is selected
+        if(this.type==="song"){
+            [header_text, body_text] = this.generate_song_info()
+            info_card.style("display","inline");
+        } else if (this.type === "artist"){
+            [header_text, body_text] = this.generate_artist_info()
+            info_card.style("display","inline");
+        } else{
+            info_card.style("display","none");
         };   
+
+        info_card.select(".info_card_header").html(header_text);
+        info_card.select(".info_card_body").html(body_text);
     }
 
-    add_song_info(info_card){
-        info_card.select(".info_card_header").text("Song title");
-        var body = info_card.select(".info_card_body")
-        body.append("p").text("Artist: ")
-            .append("p").text("Album: ")
-            .append("p").text("Key:")
-            .append("p").text("Tempo:")
-            .append("p").text("Spotify streams:")
-            .append("p").text("YouTube views:")
-            .append("p").text("YouTube interactions:")
-            .append("p").text("Spotify URL:")
-            .append("p").text("YouTube URL:");
-
+    generate_song_info(){
+        let header = "Song title"
+        let text = `
+            <div>
+                <div>Artist</div>
+                <div></div>
+            </div>
+            <div>
+                <div>Album</div>
+                <div></div>
+            </div>
+            <div>
+                <div>Key</div>
+                <div></div>
+            </div>
+            <div>
+                <div>Tempo</div>
+                <div> </div>
+            </div>
+            <div>
+                <div>Spotify streams</div>
+                <div></div>
+            </div>
+            <div>
+                <div> YouTube views </div>
+                <div> </div>
+            </div>
+            <div>
+                <div> YouTube interactions </div>
+                <div> </div>
+            </div>
+            <div>
+                <div> Spotify URL</div>
+                <div> </div>
+            </div>
+            <div>
+                <div> YouTube URL </div>
+                <div> </div>
+            </div>
+            `
+        return [header, text];
     }
 
-    add_artist_info(info_card){
-        info_card.select(".info_card_header").text("Artist name");
-        var body = info_card.select(".info_card_body")
-        body.append("p").text("Spotify streams: ")
-            .append("p").text("YouTube views: ")
-            .append("p").text("YouTube interactions:");
+    //adds the information if an artist is selected
+    generate_artist_info(){
+        let header = "Artist name";
+        let text = `
+            <div>
+                <div>Spotify streams:</div>
+                <div> </div>
+            </div>
+            <div>
+                <div>YouTube views: </div>
+                <div></div>
+            </div>
+            <div>
+                <div>YouTube interactions: </div>
+                <div> </div>
+            </div>
+            `;
+        return [header, text];
     }
-}
+
+    hide(){
+        d3.select(this.id).style("display","none");
+    }
+
+    //sets a new type: song or artist for the infocard, automatically updates infocard
+    setType(type){
+        this.type = type;
+        this.update_info()
+        //TODO set automatic data (first artist or song)
+    }
+
+    //sets new data as the data displayed on the infocard
+    setData(data){
+        this.data = data;
+    }
+} 
--- root/js/script.js
+++ root/js/script.js
@@ -1,14 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const svg = d3.select("body")
-        .append("svg")
-        .attr("width", 400)
-        .attr("height", 300);
 
-    svg.append("circle")
-        .attr("cx", 200)
-        .attr("cy", 150)
-        .attr("r", 50)
-        .attr("fill", "blue");
+    //make infocards
+    let infocard1 = new InfoCard("#info_card1", "song");
 
-    let infocard = new InfoCard("#info_cards_wrapper", "song", "dummy");
+    let infocard2 = new InfoCard("#info_card2");
+
+    // Get the radio buttons by name
+    const radioButtons1 = document.getElementsByName('radio1');
+
+    // Add event listener to each radio button
+    radioButtons1.forEach(radioButton => {
+        radioButton.addEventListener('change', function() {
+            if (this.checked) {
+                //update corresponding infocard
+                infocard1.setType(this.value)
+                //TODO update spiderchart
+            }
+        });
+    });
+
+    // Get the radio buttons by name
+    const radioButtons2 = document.getElementsByName('radio2');
+
+    // Add event listener to each radio button
+    radioButtons2.forEach(radioButton => {
+        radioButton.addEventListener('change', function() {
+            if (this.checked) {
+                //update corresponding infocard
+                infocard2.setType(this.value)
+                //TODO update spiderchart
+            }
+        });
+    });
+
 });
