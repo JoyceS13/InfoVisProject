1ae873541e9e4058b00e5ef02dfc7036e96c8bfa
Jasper Vos <jvos@9292.nl>
2023-12-12 08:48:50 +0100

    Changed radarchart color handling, added legend

--- root/js/GenreComparisonChart.js
+++ root/js/GenreComparisonChart.js
@@ -2,28 +2,28 @@ let features = ["Danceability", "Energy", "Valence", "Loudness", "Tempo"];
 let genre_chart = new RadarChart("#genre_chart");
 let genre_working_set = undefined;
 
-function draw_weighted(dataSet,chart) {
+function draw_weighted(dataSet, chart, color) {
   // Prepare data
-  const  totalViews = d3.sum(dataSet, d => d.Views);
+  const totalViews = d3.sum(dataSet, d => d.Views);
   const avgColumns = {};
   dataSet.forEach(row => {
     Object.keys(row).forEach(key => {
       if (features.includes(key)) {
-        avgColumns[key] = (avgColumns[key] || 0) + (row[key]/1);
+        avgColumns[key] = (avgColumns[key] || 0) + (row[key] / 1);
       }
     });
   });
-  for(let feature of features) {
-    avgColumns[feature] = avgColumns[feature]/dataSet.length;
+  for (let feature of features) {
+    avgColumns[feature] = avgColumns[feature] / dataSet.length;
   }
-  chart.drawData(avgColumns, 255,0,0);
+  chart.drawData(avgColumns, color);
 
 }
 
 
 function init_genre_graph(data) {
   // Setup working set for genredata.
-  const maxTempo = d3.max(data, d => d.Tempo/1);
+  const maxTempo = d3.max(data, d => d.Tempo / 1);
   genre_working_set = data.map(function (row) {
     return {
       Views: row.Views,
@@ -31,7 +31,7 @@ function init_genre_graph(data) {
       Danceability: row.Danceability,
       Energy: row.Energy,
       Valence: row.Valence,
-      Loudness: (row.Loudness)/60 + 1,
+      Loudness: (row.Loudness) / 60 + 1,
       Tempo: row.Tempo / maxTempo,
       genre: row.track_genre
     }
@@ -54,21 +54,44 @@ function init_genre_graph(data) {
 }
 
 function draw_genres(genres) {
-  genres.forEach(function(genre) {
-    genreSet = genre_working_set.filter(x => {return x.genre == genre});
-    draw_weighted(genreSet, genre_chart);
-  });
+  colors = [];
+  for (let i = 0; i < genres.length; ++i) {
+    colors.push(d3.rgb(d3.interpolateViridis(i / (genres.length - 1))));
+  }
+  genre_chart.clear();
+  d3.select("#legend").html("");
+  const legendDiv = d3.select("#legend");
+  for(let i = 0; i < genres.length; i++){
+    genreSet = genre_working_set.filter(x => { return x.genre == genres[i] });
+    draw_weighted(genreSet, genre_chart, colors[i]);
+
+    const legendItem = legendDiv.append("div")
+      .style("display", "flex")
+      .style("align-items", "center")
+      .style("margin-bottom", "8px"); // Adjust spacing as needed
+
+    legendItem.append("div")
+      .style("width", "18px")
+      .style("height", "18px")
+      .style("background-color", colors[i])
+      .style("margin-right", "8px");
+
+    legendItem.append("span")
+      .text(genres[i])
+      .style("font-size", "14px") // Adjust font size as needed
+      .style("font-weight", "500"); // Adjust font weight as needed
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   $('#items').select2({
-      placeholder: 'Select items',
-      allowClear: true,
-      width: 'resolve', // 'resolve' makes the width adjust to the container
+    placeholder: 'Select items',
+    allowClear: true,
+    width: 'resolve', // 'resolve' makes the width adjust to the container
   });
-  $('#items').on('change', function() {
-    const selectedValues = $(this).val();
-    draw_genres(genres);
-    console.log('Items selected:', selectedValues);
+  $('#items').on('change', function () {
+    const selected_genres = $(this).val();
+    draw_genres(selected_genres);
+    console.log('Items selected:', selected_genres);
   })
 });
--- root/js/RadarChart.js
+++ root/js/RadarChart.js
@@ -67,26 +67,27 @@ class RadarChart {
         this.svg = svg;
     }
 
-    drawData(data, r, g, b){
+    drawData(data, color) {
         const points = features.map((feature, i) => {
             return {
                 x: this.scale(data[feature]) * Math.cos((Math.PI / 2) + (2 * Math.PI * i / features.length)),
                 y: this.scale(data[feature]) * Math.sin((Math.PI / 2) + (2 * Math.PI * i / features.length))
             };
         });
-        
+    
         const lineGenerator = d3.line()
             .x(d => d.x)
             .y(d => d.y)
             .curve(d3.curveCardinalClosed.tension(.75)); // Change the curve type here
-
+    
         const path = this.svg.append("path")
             .datum(points)
-            .attr("fill", `rgba(${r},${g},${b}, .5)`)
-            .attr("stroke", `rgb(${r},${g},${b})`)
+            .attr("fill", color.copy({opacity: 0.5})) // Use d3 color to set opacity
+            .attr("stroke", color.toString())
             .attr("stroke-width", 2)
             .attr("d", lineGenerator);
     }
+
     draw(features, data) {
         this.drawGraph(features);
         this.drawData(data);
