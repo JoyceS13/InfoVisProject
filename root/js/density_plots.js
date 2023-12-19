ebb4847798273b37bceef17a1758a7e26e7e3e20
olgabunkova <O.A.Bunkova@student.tudelft.nl>
2023-12-19 23:12:46 +0100

    fixes

--- root/js/density_plots.js
+++ root/js/density_plots.js
@@ -1,6 +1,6 @@
-var width = 1000,
-    size = 300,
-    padding = 40;
+var width = 1300,
+    size = 250,
+    padding = 50;
 // Append "Show by genre" checkbox
 var checkboxDiv = d3.select("body")
   .append("div");
@@ -38,39 +38,44 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
 
     traitsY.forEach(function(traitY) {
         domainByTrait[traitY] = d3.extent(data, function(d) { return +d[traitY]; });
-        domainByTrait[traitY][1] = domainByTrait[traitY][1];
-        domainByTrait[traitY][0] = 10000;
+        //domainByTrait[traitY][1] = domainByTrait[traitY][1];
+       // domainByTrait[traitY][0] = 10000;
     });
 
     var svg = d3.select("body").append("svg")
-        .attr("width", size * traitsX.length + padding) // Adjusted width based on the number of x traits
-        .attr("height", size * traitsY.length + padding) // Adjusted height based on the number of y traits
+        .attr("width", size * traitsX.length + 2 * padding) // Adjusted width based on the number of x traits
+        .attr("height", size * traitsY.length + 2 * padding) // Adjusted height based on the number of y traits
         .append("g")
-        .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+        .attr("transform", "translate(" + padding + "," - padding / 2 + ")");
+    
 
+       
     var cell = svg.selectAll(".cell")
         .data(cross(traitsY, traitsX)) // Reversed order of traitsY and traitsX for the correct layout
         .enter().append("g")
         .attr("transform", function(d, i) {
-            var translateX = (i % traitsX.length) * size;
-            var translateY = (traitsY.length - Math.floor(i / traitsX.length) - 1) * size;
-            return "translate(" + translateX + "," + translateY + ")";
-        }).each(plot);
+          //var translateX = (i % traitsX.length) * (size + padding); // Adjusted translation for X
+         // var translateY = Math.floor(i / traitsX.length) * (size + padding); // Adjusted translation for Y
+         var translateX = (i % traitsX.length)  * size + 1.5 * padding;
+         var translateY = (traitsY.length - Math.floor(i / traitsX.length) - 1) * size;
+         return "translate(" + translateX + "," + translateY + ")";
+      }).each(plot);
 
     function plot(p) {
-        var x = d3.scaleLinear()  // Change to logarithmic scale
+      
+        var x = d3.scaleLinear()  
             .range([padding, size - padding]);
 
         var y = d3.scaleLinear()
-            .range([size - padding / 2 - 10, padding / 2]);
+            .range([size - padding  , padding]);
         var cell = d3.select(this);
 
         //xAxis
         var xAxis = x.domain(domainByTrait[p.x]).range([padding, size - padding]).nice();
         
         //yAxis
-        var yAxis = y.domain(domainByTrait[p.y]).range([padding, size - padding]);
-
+        var yAxis = y.domain(domainByTrait[p.y]).range([size - padding / 2 - 10, padding / 2]).nice();
+       
         // Append x-axis label
         cell.append("text")
             .attr("class", "x label")
@@ -85,34 +90,34 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
             .attr("text-anchor", "middle")
             .attr("transform", "rotate(-90)")
             .attr("x", -size / 2)
-            .attr("y", - padding / 2)
+            .attr("y", - padding )
             .text(p.y);
+        // Append a group element for brush paths
+        var brushPaths = cell.append("g").attr("class", "brush-paths");
 
         var xAx = cell.append("g")
-            .attr("transform", `translate(0, ${size - padding / 3})`)
-            .call(d3.axisBottom(x).ticks(4).tickFormat(d3.format(".1")));
+            .attr("transform", `translate(0, ${size - padding / 5})`)
+            .call(d3.axisBottom(x).ticks(4).tickFormat(formatAxisTick).tickSizeOuter(0));
         var yAx = cell.append("g")
-            .attr("transform", `translate(${padding / 2}, 0)`)
+            //.attr("transform", `translate(${padding / 5}, 0)`)
             .call(d3.axisLeft(y).ticks(5));
 
-            // var color = d3.scaleSequentialLog(d3.interpolateViridis)
-            // .domain([.1, 15]);
-            var color = d3.scaleLinear()
-              .domain([0.01, 10]) // Assuming the range of your density values
+        var color = d3.scaleLinear()
+              .domain([0.01, 4]) // Assuming the range of your density values
               .range(["#8ABF9C", "#F2D750"]); // Define your desired color range
           
         const densityData = d3.contourDensity()
             .x(function(d) { return x(d[p.x]); })
             .y(function(d) { return y(d[p.y]); })
             .size([size, size]) // Adjusted size for correct placement
-            .bandwidth(4)
+            .bandwidth(5)
             (data);
 
         cell.insert("g", "g")
             .selectAll("path")
             .data(densityData)
             .enter().append("path")
-            .attr("transform", "translate(" + padding + "," + padding / 2 + ")") // Adjusted translate
+            .attr("transform", "translate(" + padding + "," + -padding / 10 + ")") // Adjusted translate
             .attr("d", d3.geoPath())
             .attr("fill", function(d) { return color(d.value); });
     
@@ -122,12 +127,6 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
             .on("end", brushed);
         
         
-        
-            // Append brush to the plot
-        // const brushGroup = cell.append("g")
-        //   .attr("class", "brush")
-        //   .call(brush);
-
 
         cell.append("g")
             .attr("class", "brush")
@@ -140,16 +139,17 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
   
               // Update x and y domains based on brush extent
               var xDomainNew = [x.invert(x0), x.invert(x1)];
-              var yDomainNew = [y.invert(y0), y.invert(y1)];
+              var yDomainNew = [y.invert(y1), y.invert(y0)];
   
               // Update the scales with new domains
-              x.domain(xDomainNew);
-              y.domain(yDomainNew);
+              x.domain(xDomainNew).nice();
+              y.domain(yDomainNew).nice();
   
-              // Transition the axis to reflect the new domain
-              cell.select(".x").transition().duration(1000).call(d3.axisBottom(x));
-              cell.select(".y").transition().duration(1000).call(d3.axisLeft(y));
-  
+              
+
+              // Clear the brush selection
+              //d3.select(this).call(brush.move, null);
+
               // Recalculate densityData based on updated domains
               const updatedDensityData = d3.contourDensity()
                   .x(function(d) { return x(d[p.x]); })
@@ -160,14 +160,23 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
   
               // Remove existing paths
               cell.selectAll("path").remove();
-  
+              
+              // Transition axes to reflect the new domain
+              xAx.transition().duration(1000).call(d3.axisBottom(x).ticks(4).tickFormat(formatAxisTick).tickSizeOuter(0));
+              yAx.transition().duration(1000).call(d3.axisLeft(y).ticks(4));
+
+              
+
               // Enter new paths for updated density data
               cell.selectAll("path")
                   .data(updatedDensityData)
                   .enter().append("path")
-                  .attr("transform", "translate(" + padding + "," + padding / 2 + ")")
+                  .attr("transform", "translate(" + padding + "," + -padding / 10 + ")") // Adjusted translate
                   .attr("d", d3.geoPath())
                   .attr("fill", function(d) { return color(d.value); });
+
+           
+              
           }
         
        else {
@@ -177,29 +186,35 @@ d3.csv("../data/Spotify_Youtube.csv").then(function(data) {
         x.domain(domainByTrait[p.x]).nice();
         y.domain(domainByTrait[p.y]).nice();
 
-        // Transition the axis to reflect the original domain
-        cell.select(".x").transition().duration(1000).call(d3.axisBottom(x));
-        cell.select(".y").transition().duration(1000).call(d3.axisLeft(y));
 
         // Recalculate densityData based on original domains
         const updatedDensityData = d3.contourDensity()
             .x(function(d) { return x(d[p.x]); })
             .y(function(d) { return y(d[p.y]); })
             .size([size, size])
-            .bandwidth(4)
+            .bandwidth(5)
             (data);
 
         // Remove existing paths
         cell.selectAll("path").remove();
 
+        // Transition axes to reflect the new domain
+        xAx.transition().duration(1000).call(d3.axisBottom(x).ticks(4).tickFormat(formatAxisTick).tickSizeOuter(0));
+        yAx.transition().duration(1000).call(d3.axisLeft(y).ticks(4));
+
+    
         // Enter new paths for updated density data
         cell.selectAll("path")
             .data(updatedDensityData)
             .enter().append("path")
-            .attr("transform", "translate(" + padding + "," + padding / 2 + ")")
+            .attr("transform", "translate(" + padding + "," + -padding / 10 + ")") // Adjusted translate
             .attr("d", d3.geoPath())
             .attr("fill", function(d) { return color(d.value); });
-    }
+    
+    
+        
+        
+          }
 
     } 
   }
@@ -211,3 +226,9 @@ function cross(a, b) {
     return c;
 }
 
+function formatAxisTick(d) {
+  if (d === 0) {
+    return "0M";
+}
+  return d3.format('.2s')(d).replace('G', 'B').replace(/(\.0+)?G$/, 'M');
+}
